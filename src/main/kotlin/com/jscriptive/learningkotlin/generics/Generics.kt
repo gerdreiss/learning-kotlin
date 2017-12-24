2f7f97fd0bd276d295c71447fb615767b0c33767
Gerd Reiss <gerdreiss@jscriptive.com>
2017-12-24 10:37:27 +0100

    covariance

--- src/main/kotlin/com/jscriptive/learningkotlin/generics/Generics.kt
+++ src/main/kotlin/com/jscriptive/learningkotlin/generics/Generics.kt
@@ -1,11 +1,35 @@
 package com.jscriptive.learningkotlin.generics
 
+import java.math.BigDecimal
+
 
 fun main(args: Array<String>) {
     append(StringBuilder("string1 "), StringBuilder("string2 "))
+
+    println(getElementsOfType<String>(listOf("str", 1, BigDecimal.ONE)))
 }
 
 fun <T> append(item1: T, item2: T)
         where T : CharSequence, T : Appendable {
     println("Result is ${item1.append(item2)}")
+}
+
+inline fun <reified T> getElementsOfType(elms: List<Any>): List<T> =
+        elms.filter { it is T }
+                .map { it as T }
+
+
+open class Flower
+class Rose : Flower()
+
+/* covariant */
+class Garden<out T : Flower>(vararg flowers: T) {
+
+    private val _flowers: MutableList<@UnsafeVariance T> = mutableListOf(*flowers)
+
+    fun pickFlower(index: Int): T = _flowers[index]
+
+    fun plantFlower(flower: @UnsafeVariance T): Boolean =
+            _flowers.add(flower)
+
 }
