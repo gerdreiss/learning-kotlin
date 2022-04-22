dda5e24caaa982e7877d4b15be9892d2d739894c
Gerd Reiss <gerd@reiss.pro>
2022-04-22 23:12:35 +0200

    upgrade kotlin version to 1.6.21

--- fpinkotlin/src/main/kotlin/fpinkotlin/chapter01/Cafe02.kt
+++ fpinkotlin/src/main/kotlin/fpinkotlin/chapter01/Cafe02.kt
@@ -4,7 +4,7 @@ package fpinkotlin.chapter01
 class Cafe02 {
     fun buyCoffee(cc: CreditCard, p: Payments): Coffee {
         val cup = Coffee("java", 1.5)
-        cc.charge(cup.price)
+        p.charge(cc, cup.price)
         return cup
     }
 }
