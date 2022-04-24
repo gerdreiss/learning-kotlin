22487f2a0ab167a8506d837f648998c6ebb1e259
Gerd Reiss <gerd@reiss.pro>
2022-04-24 21:16:56 +0200

    [Functional Programming in Kotlin] Functional library for concurrent computation - Par type, naive impl

--- fpinkotlin/parlib/lib/src/main/kotlin/parlib/Par.kt
+++ fpinkotlin/parlib/lib/src/main/kotlin/parlib/Par.kt
@@ -1,7 +1,47 @@
 package parlib
 
-class Par<A>(val get: A)
+import java.util.concurrent.Callable
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Future
+import java.util.concurrent.TimeUnit
 
-fun <A> unit(a: () -> A): Par<A> = Par(a())
+//class Par<A>(val get: A)
 
-fun <A> get(a: Par<A>): A = a.get
+typealias Par<A> = (ExecutorService) -> Future<A>
+
+fun <A> run(es: ExecutorService, a: Par<A>): Future<A> = a(es)
+
+//fun <A> unit(a: A): Par<A> = Par(a)
+//fun <A> lazyUnit(a: () -> A): Par<A> = fork { unit(a()) }
+//fun <A, B, C> map2(a: Par<A>, b: Par<B>, f: (A, B) -> C): Par<C> = TODO()
+//fun <A> fork(a: () -> Par<A>): Par<A> = TODO()
+
+object Pars {
+    fun <A> unit(a: A): Par<A> =
+        { _: ExecutorService -> UnitFuture(a) }
+
+    data class UnitFuture<A>(val a: A) : Future<A> {
+        override fun get(): A = a
+        override fun get(timeout: Long, timeUnit: TimeUnit): A = a
+        override fun cancel(evenIfRunning: Boolean): Boolean = false
+        override fun isDone(): Boolean = true
+        override fun isCancelled(): Boolean = false
+    }
+
+    fun <A, B, C> map2(
+        a: Par<A>,
+        b: Par<B>,
+        f: (A, B) -> C
+    ): Par<C> =
+        { es: ExecutorService ->
+            val af: Future<A> = a(es)
+            val bf: Future<B> = b(es)
+            UnitFuture(f(af.get(), bf.get()))
+        }
+
+    fun <A> fork(a: () -> Par<A>): Par<A> =
+        { es: ExecutorService ->
+            es.submit(Callable { a()(es).get() })
+        }
+}
+
